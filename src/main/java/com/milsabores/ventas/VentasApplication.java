7fabc966dc8ed62c8b46cbf6087b9d09f85d4b9d
kyaragat <kia.gatica@duocuc.cl>
2025-11-27 03:40:33 -0300

    AGREGUE WALLES

--- src/main/java/com/milsabores/ventas/VentasApplication.java
+++ src/main/java/com/milsabores/ventas/VentasApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VentasApplication {
 
 	public static void main(String[] args) {
+		// Configurar las propiedades del sistema para Oracle Wallet
+		String walletPath = System.getProperty("user.dir") + "/src/main/resources/Wallet";
+		System.setProperty("oracle.net.tns_admin", walletPath);
+		System.setProperty("oracle.net.wallet_location", walletPath);
+		
 		SpringApplication.run(VentasApplication.class, args);
 	}
 
